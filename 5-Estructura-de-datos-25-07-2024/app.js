30f1ccb66468b0006b21d59a7e44da6bd33517cc
Carolain Rincon <juliethrincon9505@gmail.com>
2024-07-25 16:37:15 -0500

    ejercicio de estructura de datos en clase, aun falta terminar imagenes de los array

---
+++ 5-Estructura-de-datos-25-07-2024/app.js
@@ -0,0 +1,313 @@
+//const peliculas = ['uno', 'dos', 'tres']
+//los objetos siempre van, propiedad y valor, siempre debes tener en cuenta la sintaxis
+/* id, nombre, year, genero, image*/
+let cancionesRock = [
+   //* {
+       // id: 1,
+        //nombre: 'Twisters',
+        //year: 2024,
+        //direccion: 'Lee Isaac Chung',
+        //guionistas: ['Mark L. Smith','Michael Crichton','Joseph Kosinski'],
+        //genero: 'acción',
+        //image: 'https://upload.wikimedia.org/wikipedia/en/2/24/Twisters_Official_US_Theatrical_Poster.jpg'
+    //},
+    {
+        id: 1,
+        nombre: 'Stairway to Heaven',
+        band: 'Led Zeppelin',
+        year: 1971,
+        genero: 'Rock / Hard Rock',
+        image: ''
+    },
+    {
+        id: 2,
+        nombre: 'Bohemian Rhapsody',
+        band: 'Queen',
+        year: 1975,
+        genero: 'Rock / Opera Rock',
+        image: ''
+    },
+    {
+        id: 3,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 4,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 5,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 6,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 7,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 8,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 9,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 10,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 11,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 12,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 13,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 14,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 15,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 16,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 17,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 18,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 19,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 20,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 21,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 22,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 23,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 24,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 25,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 26,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 27,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 28,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 29,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+    {
+        id: 30,
+        nombre: ''
+        year: ''
+        genero: ''
+        image: ''
+    },
+
+]
+
+"Smells Like Teen Spirit" – Nirvana (1991)
+
+Género: Grunge / Alternative Rock
+"Enter Sandman" – Metallica (1991)
+
+Género: Heavy Metal / Thrash Metal
+"Hotel California" – Eagles (1976)
+
+Género: Rock / Classic Rock
+"Sweet Child o' Mine" – Guns N' Roses (1987)
+
+Género: Hard Rock
+"Back in Black" – AC/DC (1980)
+
+Género: Hard Rock / Heavy Metal
+"Paranoid" – Black Sabbath (1970)
+
+Género: Heavy Metal
+"Iron Man" – Black Sabbath (1970)
+
+Género: Heavy Metal
+"Another Brick in the Wall (Part 2)" – Pink Floyd (1979)
+
+Género: Progressive Rock
+"Kashmir" – Led Zeppelin (1975)
+
+Género: Rock / Hard Rock
+"One" – Metallica (1988)
+
+Género: Heavy Metal / Thrash Metal
+"November Rain" – Guns N' Roses (1991)
+
+Género: Hard Rock
+"Highway to Hell" – AC/DC (1979)
+
+Género: Hard Rock / Heavy Metal
+"Creep" – Radiohead (1992)
+
+Género: Alternative Rock / Grunge
+"Fade to Black" – Metallica (1984)
+
+Género: Heavy Metal / Thrash Metal
+"You Shook Me All Night Long" – AC/DC (1980)
+
+Género: Hard Rock / Heavy Metal
+"Livin' on a Prayer" – Bon Jovi (1986)
+
+Género: Rock / Glam Metal
+"Roxanne" – The Police (1978)
+
+Género: Rock / New Wave
+"Run to the Hills" – Iron Maiden (1982)
+
+Género: Heavy Metal
+"Welcome to the Jungle" – Guns N' Roses (1987)
+
+Género: Hard Rock
+"Don't Stop Believin'" – Journey (1981)
+
+Género: Rock / Arena Rock
+"Master of Puppets" – Metallica (1986)
+
+Género: Heavy Metal / Thrash Metal
+"Thunderstruck" – AC/DC (1990)
+
+Género: Hard Rock / Heavy Metal
+"Under the Bridge" – Red Hot Chili Peppers (1992)
+
+Género: Alternative Rock / Funk Rock
+"Hallowed Be Thy Name" – Iron Maiden (1982)
+
+Género: Heavy Metal
+"Smells Like Teen Spirit" – Nirvana (1991)
+
+Género: Grunge / Alternative Rock
+"Sweet Emotion" – Aerosmith (1975)
+
+Género: Rock / Hard Rock
+"Dream On" – Aerosmith (1973)
+
+Género: Rock / Hard Rock
+"People Are Strange" – The Doors (1967)
+
+Género: Rock / Psychedelic Rock
+Estas canciones han dejado una marca duradera en la historia del rock y el metal, y continúan siendo influyentes en la música moderna. ¡Espero que disfrutes explorando estas pistas!
