3590686e882d3d31fac1e51bb005fb5c29d397a8
Carolain Rincon <juliethrincon9505@gmail.com>
2024-08-28 14:59:37 -0500

    index terminado en html y css

---
+++ 10-PROYECTO-APP/js/app.js
@@ -0,0 +1,17 @@
+/* function toggleMenu() {
+    var dropdown = document.getElementById("dropdownContent");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
+function toggleProfileMenu() {
+    var profileMenu = document.getElementById("profileMenuContent");
+    if (profileMenu.style.display === "block") {
+        profileMenu.style.display = "none";
+    } else {
+        profileMenu.style.display = "block";
+    }
+} */
